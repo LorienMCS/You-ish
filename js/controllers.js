a05e6d73537ae04937f56281d5b3b2eadde15671
Lorien <LorienMCS@gmail.com>
2015-09-16 16:24:30 -0700

    add movie search

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,15 @@
-app.controller('YouishController', ['$scope', 'GiphyService', function($scope, GiphyService) {
+app.controller('YouishController', ['$scope', 'GiphyService', 'ImdbService', function($scope, GiphyService, ImdbService) {
 	$scope.firstName = "";
 	$scope.month = "";
 	$scope.day = "";
 	$scope.giphy = "";
 	$scope.noGiphy = true;
-	$scope.noError = true;
-	$scope.error = "";
+	$scope.noGiphyError = true;
+	$scope.giphyError = "";
+	$scope.movies = [];
+	$scope.noMovie = true;
+	$scope.noMovieError = true;
+	$scope.movieError = "";
 
 	$scope.getGiphy = function() {
 		GiphyService.search($scope.firstName)
@@ -15,12 +19,42 @@ app.controller('YouishController', ['$scope', 'GiphyService', function($scope, G
 				$scope.giphy = obj.data[0].images.fixed_height.url;
 			} else {
 				$scope.noGiphy = true;
-				$scope.noError = false;
-				$scope.error = "Sorry, no Giphy for your name!"
+				$scope.noGiphyError = false;
+				$scope.giphyError = "Sorry, no Giphy for your name!"
 			}
 		});
 	};
+
+	$scope.searchImdb = function() {
+		ImdbService.search($scope.firstName)
+		.then(function(obj) {
+			if(!obj.Error && obj.Search[0]!=undefined && obj.Search.length !== 0){
+				$scope.noMovie = false;
+				$scope.movies = [];
+				var objArr = obj.Search;
+				objArr.forEach(function(movie){
+					$scope.movies.push({'title': movie.Title, 'year': movie.Year, 'id': movie.imdbID});
+				});
+			} else {
+				$scope.noMovie = true;
+				$scope.noMovieError = false;
+				$scope.movieError = "Sorry, no movies for your name!"
+			};
+		});
+	};
+
+	$scope.searchAll = function() {
+		$scope.getGiphy();
+		$scope.searchImdb();
+	}
+
+
 }]);
+
+
+
+
+
 
 
 app.controller('AboutController', ['$scope', function($scope) {
--- js/services.js
+++ js/services.js
@@ -32,3 +32,38 @@ app.factory('GiphyService', ["$http", "$q", function($http, $q) {
 
   return GiphyService;
 }]);
+
+
+app.factory('ImdbService', ["$http", "$q", function($http, $q) {
+  var ImdbService = {};
+  var baseUrl = "http://www.omdbapi.com/?s=";
+  var searchTerm = '';
+
+  ImdbService.setSearchTerm = function(term) {
+    searchTerm = encodeURIComponent(term);
+  }
+
+  ImdbService.getSearchTerm = function() {
+    return decodeURIComponent(searchTerm);
+  }
+
+  ImdbService.search = function(term,cb) {
+    if (term !== undefined) {
+      ImdbService.setSearchTerm(term);
+    }
+
+    var url = baseUrl + searchTerm;
+
+    var deferred = $q.defer();
+
+    $http.get(url).success(function(data) {
+      deferred.resolve(data);
+    }).error(function() {
+      deferred.reject("Cannot Get Movies")
+    });
+
+    return deferred.promise;
+  }
+
+  return ImdbService;
+}]);
