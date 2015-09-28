cb0114f247902989578b4970b14eb73a816eacaf
Lorien <LorienMCS@gmail.com>
2015-09-28 14:08:15 -0700

    update reverse function

--- js/filters.js
+++ js/filters.js
@@ -1,11 +1,10 @@
 app.filter('reverse', function() {
   return function(input) {
-    input = input || '';
     var reversed = "";
     var output = "";
-    var wordArray = input.split("");
-    wordArray.reverse();
-    reversed = wordArray.join("");
+    for(var i = input.length-1; i >= 0; i--) {
+        reversed += input.charAt(i);
+    }
     output = reversed.charAt(0).toUpperCase() + reversed.substring(1).toLowerCase();
     return output;
   };
